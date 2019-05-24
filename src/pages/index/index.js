e9d9708b7c7f854bf314d9ff82f40f316677b73b
madness <madnessJs@yandex.ru>
2019-05-24 16:20:23 +0300

    Компонент 'Форма поиска отеля'
    разметка, стилизация и начало оживления с помощью плагинов js

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,20 @@
-import $ from 'jquery';
+import 'air-datepicker';
+
+var $start = $('#find-room__from'),
+    $end = $('#find-room__to');
+
+$start.datepicker({
+    onSelect: function (fd, date) {
+        $end.data('datepicker')
+                .update('minDate', date);
+
+        $end.focus();
+    }
+})
+
+$end.datepicker({
+    onSelect: function (fd, date) {
+        $start.data('datepicker')
+                .update('maxDate', date)
+    }
+})
