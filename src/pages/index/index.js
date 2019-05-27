1de3c3ccbab02ba93bde2a50cb9cecd7cea8605f
madness <madnessJs@yandex.ru>
2019-05-27 15:56:58 +0300

    Завершение компонента "Календарь" и общие правки

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,20 +1,32 @@
-import 'air-datepicker';
+$('.js-find-room__from').datepicker({
+    clearButton: true,
+    todayButton: true,
+    prevHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(-15%,-15%) rotate(180deg); transform-origin: center center" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
+    nextHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(15%,15%)" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
+    navTitles: {
+        days: 'MM yyyy'
+    },
 
-var $start = $('#find-room__from'),
-    $end = $('#find-room__to');
-
-$start.datepicker({
-    onSelect: function (fd, date) {
-        $end.data('datepicker')
-                .update('minDate', date);
-
-        $end.focus();
-    }
+    minDate: new Date(),
+    offset: 0,
+    // inline: true
 })
 
-$end.datepicker({
-    onSelect: function (fd, date) {
-        $start.data('datepicker')
-                .update('maxDate', date)
-    }
-})
+// var $start = $('#find-room__from'),
+//     $end = $('#find-room__to');
+
+// $start.datepicker({
+//     onSelect: function (fd, date) {
+//         $end.data('datepicker')
+//                 .update('minDate', date);
+
+//         $end.focus();
+//     }
+// })
+
+// $end.datepicker({
+//     onSelect: function (fd, date) {
+//         $start.data('datepicker')
+//                 .update('maxDate', date)
+//     }
+// })
