95449d36820f400621e3e7c6ed8c59ba6752e4a3
madness <madnessJs@yandex.ru>
2019-06-11 15:49:32 +0300

    Перенес сгруппировал папки форм в одной папке и
    завершил компоненты - "карточка подтверждения номера" и "карточка
    регистрации"

--- src/pages/components/components.js
+++ src/pages/components/components.js
@@ -1,5 +1,7 @@
 // Datepicker
-$('#find-room__from').datepicker({
+var $start = $('#find-room__from'), $end = $('#find-room__to');
+
+$start.datepicker({
     clearButton: true,
     todayButton: true,
     prevHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(-15%,-15%) rotate(180deg); transform-origin: center center" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
@@ -7,10 +9,39 @@ $('#find-room__from').datepicker({
     navTitles: {
         days: 'MM yyyy'
     },
-
     minDate: new Date(),
     offset: 0,
-    //inline: true
+    //inline: true,
+    onSelect: function (fd, date) {
+        $end.data('datepicker')
+                .update({
+                    'minDate': date,
+                    'minRange': date
+                });
+
+        $end.focus();
+    },
+})
+
+$end.datepicker({
+    clearButton: true,
+    todayButton: true,
+    prevHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(-15%,-15%) rotate(180deg); transform-origin: center center" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
+    nextHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(15%,15%)" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
+    navTitles: {
+        days: 'MM yyyy'
+    },
+    offset: 0,
+    //inline: true,
+    onSelect: function (fd, date) {
+        $start.data('datepicker')
+                .update({
+                    'maxDate': date,
+                    'maxRange': date
+                });
+
+        $end.blur();
+    }
 })
 
 
@@ -219,22 +250,3 @@ for (var k = 0; k < inputDropdownComfort.length; k++) {
         }
     });
 }
-
-// var $start = $('#find-room__from'),
-//     $end = $('#find-room__to');
-
-// $start.datepicker({
-//     onSelect: function (fd, date) {
-//         $end.data('datepicker')
-//                 .update('minDate', date);
-
-//         $end.focus();
-//     }
-// })
-
-// $end.datepicker({
-//     onSelect: function (fd, date) {
-//         $start.data('datepicker')
-//                 .update('maxDate', date)
-//     }
-// })
