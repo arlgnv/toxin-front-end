5e679fa311fd8395ad7a9d3a8da419bdfb0a26f7
madness <madnessJs@yandex.ru>
2019-06-12 16:50:24 +0300

    Компонент main-header - pixel perfect

--- src/pages/components/components.js
+++ src/pages/components/components.js
@@ -1,5 +1,5 @@
 // Datepicker
-var $start = $('#find-room__from'), $end = $('#find-room__to');
+var $start = $('#find-room__from');
 
 $start.datepicker({
     clearButton: true,
@@ -11,37 +11,12 @@ $start.datepicker({
     },
     minDate: new Date(),
     offset: 0,
-    //inline: true,
-    onSelect: function (fd, date) {
-        $end.data('datepicker')
-                .update({
-                    'minDate': date,
-                    'minRange': date
-                });
-
-        $end.focus();
+    range: true,
+    multipleDatesSeparator: "-",
+    onSelect: function (fd, date, picker) {
+        $start.val(fd.split("-")[0]);
+        $('#find-room__to').val(fd.split("-")[1]);
     },
-})
-
-$end.datepicker({
-    clearButton: true,
-    todayButton: true,
-    prevHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(-15%,-15%) rotate(180deg); transform-origin: center center" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
-    nextHtml: '<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path style="transform:translate(15%,15%)" d="M8.363.984L16.378 9l-8.015 8.016-1.407-1.407 5.578-5.625H.347V8.016h12.187L6.956 2.39 8.363.984z" fill="#BC9CFF"/></svg>',
-    navTitles: {
-        days: 'MM yyyy'
-    },
-    offset: 0,
-    //inline: true,
-    onSelect: function (fd, date) {
-        $start.data('datepicker')
-                .update({
-                    'maxDate': date,
-                    'maxRange': date
-                });
-
-        $end.blur();
-    }
 })
 
 
