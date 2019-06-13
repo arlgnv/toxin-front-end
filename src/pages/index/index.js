43ca39c8b9fe4d3992a07a7f2aa7cac79208a3cd
madness <madnessJs@yandex.ru>
2019-06-13 10:10:17 +0300

    Компонент 'main-footer-rich' - pixel perfect и
    завершение сборки главной страницы сайта

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,5 @@
 // Datepicker
 var $start = $('#find-room__from'), $end = $('#find-room__to');
-
 $start.datepicker({
     clearButton: true,
     todayButton: true,
@@ -18,3 +17,96 @@ $start.datepicker({
         $('#find-room__to').val(fd.split("-")[1]);
     },
 })
+
+
+//Dropdown Guests
+var inputDropdownGuests = document.querySelectorAll('.js-dropdown-guests');
+for (var i = 0; i < inputDropdownGuests.length; i++) {
+    inputDropdownGuests[i].addEventListener('click', function() {
+        var input = this;
+        var parent = input.closest('.dropdown-wrapper');
+        var dropdownContent = parent.querySelector('.dropdown__content');
+
+        var dropdownItems = dropdownContent.querySelectorAll('.dropdown__item:not(.dropdown__buttons)');
+        var dropdownAmouts = dropdownContent.querySelectorAll('.dropdown__amount');
+        var dropdownDelButtons = dropdownContent.querySelectorAll('.dropdown__del');
+        var dropdownInsButtons = dropdownContent.querySelectorAll('.dropdown__ins');
+
+        for (var i = 0; i < dropdownAmouts.length; i++) {
+            dropdownDelButtons[i].addEventListener('click', subtractOne);
+            dropdownInsButtons[i].addEventListener('click', insertOne);
+        }
+
+        var dropdownClear = dropdownContent.querySelector('.dropdown__clear');
+        dropdownClear.addEventListener('click', clearEntry);
+
+        var dropdownApply = dropdownContent.querySelector('.dropdown__apply');
+        dropdownApply.addEventListener('click', applyChanges);
+
+        dropdownContent.style.display = 'block';
+
+
+        function subtractOne() {
+            var dropdownAmount = this.nextElementSibling;
+            if (dropdownAmount.innerHTML == 1) {
+                dropdownAmount.innerHTML = dropdownAmount.innerHTML - 1;
+                this.style = '';
+                checkFields(dropdownAmouts, dropdownClear);
+            } else if (dropdownAmount.innerHTML > 1) {
+                dropdownAmount.innerHTML = dropdownAmount.innerHTML - 1;
+            }
+        }
+
+        function insertOne() {
+            var dropdownAmount = this.previousElementSibling;
+            dropdownAmount.innerHTML = +dropdownAmount.innerHTML + 1;
+
+            var dropdownDel = dropdownAmount.previousElementSibling;
+            dropdownDel.style.opacity = '1';
+
+            dropdownClear.style.display = 'block';
+        }
+
+        function clearEntry() {
+            for (var i = 0; i < dropdownAmouts.length; i++) {
+                dropdownAmouts[i].innerHTML = 0;
+                dropdownDelButtons[i].style = '';
+            }
+            this.style.display = 'none';
+        }
+
+        function applyChanges() {
+            input.value = '';
+            var amountGuests = 0, amountBabies = 0, amountBabiesText;
+            for (var i = 0; i < dropdownItems.length; i++) {
+                var amount = dropdownItems[i].querySelector('.dropdown__amount').innerHTML;
+                if (dropdownItems[i].querySelector('.dropdown__title').innerHTML == "Младенцы") amountBabies += +amount;
+                amountGuests += +amount;
+            }
+
+            if (amountBabies == 1) amountBabiesText = ' младенец';
+            else if (amountBabies >= 2 && amountBabies <= 4) amountBabiesText = ' младенца';
+            else if (amountBabies >= 5 || amountBabies == 0) amountBabiesText = ' младенцев';
+
+            if (amountGuests == 1) input.value = amountGuests + ' гость, ' + amountBabies + amountBabiesText;
+            else if (amountGuests >= 2 && amountGuests <= 4) input.value = amountGuests + ' гостя, ' + amountBabies + amountBabiesText;
+            else if (amountGuests >= 5 || amountGuests == 0) input.value = amountGuests + ' гостей, ' + amountBabies + amountBabiesText;
+
+            dropdownContent.style = '';
+
+            for (var i = 0; i < dropdownAmouts.length; i++) {
+                dropdownDelButtons[i].removeEventListener('click', subtractOne);
+                dropdownInsButtons[i].removeEventListener('click', insertOne);
+            }
+        }
+
+        function checkFields(fieldsArr, clearButton) {
+            var flag = true;
+            for (var i = 0; i < fieldsArr.length; i++) {
+                if (fieldsArr[i].innerHTML > 0) flag = false;
+            }
+
+            if (flag) clearButton.style = '';
+        }
+    });
+}
